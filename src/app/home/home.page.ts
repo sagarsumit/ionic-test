529c1db2cf001408516296314d7783afbb0801f8
mitzsu <timusiiit123@gmail.com>
2019-10-21 23:00:05 +0530

    Button clicked event is added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  text = "Default Ionic Text";
   constructor() {}
-
+  onChangeText(){
+    this.text = "Thanks to Ionic!";
+  }
 }
